49f365f384429116614e1d2062c90cb104164033
Joshua Prettyman <j.prettyman@icloud.com>
2025-10-01 17:25:25 +0200

    Renaming files and re-organising into folders

--- scrabbled_alphabet.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-if __name__ == "__main__":
-    a=sys.argv[1]
-    l=sys.argv[2:]
-    tups = [(tuple(a.index(c) for c in w), w) for w in l]
-    print(" ".join(t[1] for t in sorted(tups)))
